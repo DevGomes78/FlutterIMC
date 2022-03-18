5d2681e335e6ce1d6ad3abbf3098d10ab36c296a
amilson gomes <gomes.amilson@gmail.com>
2022-03-17 21:51:26 -0300

    Componentizado o widget Text atualizado funcao Calcular imc

--- lib/pages/page_imc.dart
+++ lib/pages/page_imc.dart
@@ -19,125 +19,130 @@ class _PageImcState extends State<PageImc> {
   void _resetcampos() {
     pesoController.text = '';
     alturaController.text = '';
+    _infoText = '';
     setState(() {
       _formKey = GlobalKey<FormState>();
     });
   }
+
   void CalcImc() {
     setState(() {
       double peso = double.parse(pesoController.text);
       double altura = double.parse(alturaController.text) / 100;
       double imc = peso / (altura * altura);
 
-      if (imc < 18.6) {
-        _infoText = "Abaixo do peso ${imc.toStringAsPrecision(3)}";
-      } else if (imc >= 18.6 && imc < 24.9) {
-        _infoText = "Peso ideal ${imc.toStringAsPrecision(3)}";
-      } else if (imc >= 24.9 && imc < 29.9) {
-        _infoText = "acima do Peso ${imc.toStringAsPrecision(3)}";
-      } else if (imc >= 29.9 && imc < 34.9) {
-        _infoText = "Obesidade grau I ${imc.toStringAsPrecision(3)}";
-      } else if (imc >= 34.9 && imc < 39.9) {
-        _infoText = "Obesidade grau II ${imc.toStringAsPrecision(3)}";
-      } else if (imc >= 40) {
-        _infoText = "Obesidade grau III ${imc.toStringAsPrecision(3)}";
+        if (imc < 18.6) {
+          _infoText = "Abaixo do peso ${imc.toStringAsPrecision(3)}";
+        } else if (imc >= 18.6 && imc < 24.9) {
+          _infoText = "Peso ideal ${imc.toStringAsPrecision(3)}";
+        } else if (imc >= 24.9 && imc < 29.9) {
+          _infoText = "acima do Peso ${imc.toStringAsPrecision(3)}";
+        } else if (imc >= 29.9 && imc < 34.9) {
+          _infoText = "Obesidade grau I ${imc.toStringAsPrecision(3)}";
+        } else if (imc >= 34.9 && imc < 39.9) {
+          _infoText = "Obesidade grau II ${imc.toStringAsPrecision(3)}";
+        } else if (imc >= 40) {
+          _infoText = "Obesidade grau III ${imc.toStringAsPrecision(3)}";
+        }
       }
-    });
-  }
+      );
+    }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Calculadora de IMC'),
-        centerTitle: true,
-        actions: [
-          IconButton(
-              onPressed: _resetcampos,
-              icon: const Icon(
-                Icons.refresh,
-                color: Colors.white,
-              ))
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 50,
-              ),
-              Container(
-                height: 200,
-                child: Image.asset(
-                  'images/coracao.png',
+        @override
+        Widget build(BuildContext context)
+    {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Calculadora de IMC'),
+          centerTitle: true,
+          backgroundColor: Color(0xFFF1A7DFF),
+          actions: [
+            IconButton(
+                onPressed: _resetcampos,
+                icon: const Icon(
+                  Icons.refresh,
                   color: Colors.white,
+                ))
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 50,
                 ),
-              ),
-              TextWidget(
-                'Peso',
-                'Informe o Peso',
-                pesoController,
-                TextInputType.number,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Informe seu Peso!';
-                  }
-                },
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextWidget(
-                'Altura',
-                'Informe a altura',
-                alturaController,
-                TextInputType.number,
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'insira sua altura!';
-                  }
-                },
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              buildGestureDetector(),
-              const SizedBox(height: 20),
-              Text(
-                _infoText,
-                style: const TextStyle(
-                  fontSize: 22,
-                  color: Colors.white,
+                Container(
+                  height: 200,
+                  child: Image.asset(
+                    'images/coracao.png',
+                    color: Colors.red,
+                  ),
                 ),
-              ),
-            ],
+                TextWidget(
+                  'Peso',
+                  'Informe o Peso',
+                  pesoController,
+                  TextInputType.number,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Informe seu Peso!';
+                    }
+                  },
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextWidget(
+                  'Altura',
+                  'Informe a altura',
+                  alturaController,
+                  TextInputType.number,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'insira sua altura!';
+                    }
+                  },
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                buildGestureDetector(),
+                const SizedBox(height: 20),
+                Text(
+                  _infoText,
+                  style: const TextStyle(
+                    fontSize: 22,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
-      ),
-    );
-  }
+      );
+    }
 
-  buildGestureDetector() {
-    return GestureDetector(
-      onTap:  CalcImc,
-      child: Container(
-        alignment: Alignment.center,
-        height: 50,
-        width: double.infinity,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: Colors.white,
-        ),
-        child: const Text(
-          'calcular',
-          style: TextStyle(
-            fontSize: 22,
-            color: Colors.black,
+    buildGestureDetector() {
+      return GestureDetector(
+        onTap: CalcImc,
+        child: Container(
+          alignment: Alignment.center,
+          height: 50,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Color(0xFFF1A7DFF),
+          ),
+          child: const Text(
+            'calcular',
+            style: TextStyle(
+              fontSize: 22,
+              color: Colors.white,
+            ),
           ),
         ),
-      ),
-    );
+      );
+    }
   }
-}
